ddde5f478d83e28478702e35a6b20b7a739751f0
Jeferson Almeida <jeferson.live11@gmail.com>
2025-02-27 15:18:04 -0300

    criei meu while

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+
+condicao = True
+
+while condicao:
+    print("Execute minha ETL")
+    time.sleep(5)
+
+    ##lista_de_alunos = ["rafael", "fábio", "luciano"]
+
+    ##for aluno in lista_de_alunos:
+        ##print(aluno)
